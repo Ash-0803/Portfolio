c0942054f38a736cb7b21d8367fabbf240de6d3d
Ashish Chaudhary <ashish080303@gmail.com>
2023-01-05 16:09:39 +0530

    Added images and completed website

---
+++ script.js
@@ -0,0 +1,33 @@
+const header = document.querySelector('#header-project');
+
+fetch('./projectsData.json')
+.then((response)=>response.json())
+.then((json)=> addHtml(json));
+
+// addHtml(); 
+function addHtml(json){
+  const array = json.array; 
+
+  for(var i=0; i<array.length; i++){
+    let value = array[i];
+    header.innerHTML += `
+    <div class="all-contain">
+        <div class="container">
+          <img src="${value.imgLink}" alt="project image" />
+          <div class="small-contain">
+            <h2 class="title">${value.title}</h2>
+            <p class="description">
+              ${value.description}
+            </p>
+            <div class="buttons">
+              <a href="${value.srcCode}"><button class="live">Source Code</button></a>
+              <a href="${value.liveDemo}"><button class="source">Live Demo</button></a>
+            </div>
+          </div>
+        </div>
+        <hr class="line" />
+      </div>`
+  }
+}
+
+{/*  */}
